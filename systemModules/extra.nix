51e75d67728752d299a2245ddfdc20d79457adbe
very-blank <aapeli.saarelainen.76@gmail.com>
2025-06-24 11:21:57 +0300

    feat: laptop things

--- systemModules/extra.nix
+++ systemModules/extra.nix
@@ -1,5 +1,35 @@
 { ... }: 
 
 {
-  #System spesific stuff
+  services = {
+    blueman.enable = true;
+
+    actkbd = {
+      enable = true;
+      bindings = [
+        { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 5"; }
+        { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 5"; }
+      ];
+    };
+  };
+
+  hardware = {
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
+
+    tuxedo-rs = {
+      enable = true;
+      tailor-gui.enable = true;
+    };
+
+    tuxedo-drivers.enable = true;
+  };
+  
+  programs = {
+    light = {
+      enable = true;
+    };
+  };
 }
